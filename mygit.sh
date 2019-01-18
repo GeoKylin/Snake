df4dc1696582ced6b74e370dc0fb45c2dde54d59
GeoFox-bingyan <wangkai_geo@163.com>
2019-01-18 20:23:03 +0800

    2019/1/18

---
+++ mygit.sh
@@ -0,0 +1,11 @@
+# git init
+
+git add *.cpp *.h *.sh 贪吃蛇*
+
+git status
+
+git commit -m "2019/1/18"
+git remote add origin git@github.com:GeoKylin/Snake.git
+
+git pull --rebase origin master
+git push -u origin master
